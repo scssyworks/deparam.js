220006ede4c9efd69af5b1a6e1c24c3db3fa44b3
Sachin Singh <ssingh259@sapient.com>
2020-05-30 17:51:27 +0530

    #17 Bumped version

--- dist/esm/deparam.esm.js
+++ dist/esm/deparam.esm.js
@@ -3,186 +3,246 @@
  * Released under MIT license
  * @name Deparam.js
  * @author Sachin Singh <contactsachinsingh@gmail.com>
- * @version 2.1.2
+ * @version 2.1.3
  * @license MIT
  */
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
 // Vars
-const isBrowser = typeof window !== "undefined";
+var isBrowser = typeof window !== "undefined"; // Shorthand for built-ins
 
-// Shorthand for built-ins
-const isArr = Array.isArray;
-
+var isArr = Array.isArray;
 /**
  * Checks if input is a number
  * @param {*} key 
  */
-function isNumber(key) {
-    key = (key + "").trim();
-    if (['null', 'undefined', ''].indexOf(key) > -1) return false;
-    return !isNaN(Number(key));
-}
 
+function isNumber(key) {
+  key = (key + "").trim();
+  if (['null', 'undefined', ''].indexOf(key) > -1) return false;
+  return !isNaN(Number(key));
+}
 /**
  * Checks if key is a true object
  * @param {*} key Any type of value
  */
-function isObject(key) {
-    return (key != null && !isArr(key) && key.toString() === "[object Object]");
-}
 
+
+function isObject(key) {
+  return key != null && !isArr(key) && key.toString() === "[object Object]";
+}
 /**
  * Checks if query parameter key is a complex notation
  * @param {string} q 
  */
-function ifComplex(q) {
-    return (/\[/.test(q));
-}
 
+
+function ifComplex(q) {
+  return /\[/.test(q);
+}
 /**
  * Sets default value
  * @param {*} value Any value
  * @param {*} defaultValue Default value if value is undefined
  */
-function setDefault(value, defaultValue) {
-    return typeof value === 'undefined' ? defaultValue : value;
-}
 
+
+function setDefault(value, defaultValue) {
+  return typeof value === 'undefined' ? defaultValue : value;
+}
 /**
  * Converts query string to JavaScript object
  * @param {string} qs query string argument (defaults to url query string)
  */
-function deparam(qs, coerce) {
-    qs = setDefault(qs, (isBrowser ? window.location.search : "")).trim();
-    coerce = setDefault(coerce, true);
-    if (qs.charAt(0) === "?") {
-        qs = qs.replace("?", "");
-    }
-    const queryParamList = qs.split("&");
-    const queryObject = {};
-    if (qs) {
-        queryParamList.forEach((qq) => {
-            const qArr = qq.split("=").map(part => decodeURIComponent(part));
-            if (ifComplex(qArr[0])) {
-                complex.apply(this, [].concat(qArr).concat([queryObject, coerce]));
-            } else {
-                simple.apply(this, [qArr, queryObject, false, coerce]);
-            }
-        });
-    }
-    return queryObject;
-}
 
+
+function deparam(qs, coerce) {
+  var _this = this;
+
+  qs = setDefault(qs, isBrowser ? window.location.search : "").trim();
+  coerce = setDefault(coerce, true);
+
+  if (qs.charAt(0) === "?") {
+    qs = qs.replace("?", "");
+  }
+
+  var queryParamList = qs.split("&");
+  var queryObject = {};
+
+  if (qs) {
+    queryParamList.forEach(function (qq) {
+      var qArr = qq.split("=").map(function (part) {
+        return decodeURIComponent(part);
+      });
+
+      if (ifComplex(qArr[0])) {
+        complex.apply(_this, [].concat(qArr).concat([queryObject, coerce]));
+      } else {
+        simple.apply(_this, [qArr, queryObject, false, coerce]);
+      }
+    });
+  }
+
+  return queryObject;
+}
 /**
  * Converts an array to an object
  * @param {array} arr 
  */
-function toObject(arr) {
-    var convertedObj = {};
-    if (isArr(arr)) {
-        arr.forEach((value, index) => {
-            convertedObj[index] = value;
-        });
-    }
-    return convertedObj;
-}
 
+
+function toObject(arr) {
+  var convertedObj = {};
+
+  if (isArr(arr)) {
+    arr.forEach(function (value, index) {
+      convertedObj[index] = value;
+    });
+  }
+
+  return convertedObj;
+}
 /**
  * Resolves an array to object if required
  * @param {array} ob An array object
  * @param {boolean} isNumber flag to test if next key is number
  */
-function resolve(ob, isNextNumber) {
-    if (typeof ob === "undefined") return isNextNumber ? [] : {};
-    return isNextNumber ? ob : toObject(ob);
-}
 
+
+function resolve(ob, isNextNumber) {
+  if (typeof ob === "undefined") return isNextNumber ? [] : {};
+  return isNextNumber ? ob : toObject(ob);
+}
 /**
  * Resolves the target object for next iteration
  * @param {Object} ob current reference object
  * @param {string} nextProp reference property in current object
  */
-function resolveObj(ob, nextProp) {
-    if (isObject(ob)) return { ob };
-    if (isArr(ob) || typeof ob === "undefined") return { ob: resolve(ob, isNumber(nextProp)) };
-    return { ob: [ob], push: ob !== null };
-}
 
+
+function resolveObj(ob, nextProp) {
+  if (isObject(ob)) return {
+    ob: ob
+  };
+  if (isArr(ob) || typeof ob === "undefined") return {
+    ob: resolve(ob, isNumber(nextProp))
+  };
+  return {
+    ob: [ob],
+    push: ob !== null
+  };
+}
 /**
  * Handles complex query parameters
  * @param {string} key 
  * @param {string} value 
  * @param {Object} obj 
  */
-function complex(key, value, obj, doCoerce) {
-    doCoerce = setDefault(doCoerce, true);
-    const match = key.match(/([^\[]+)\[([^\[]*)\]/) || [];
-    if (match.length === 3) {
-        let prop = match[1];
-        let nextProp = match[2];
-        key = key.replace(/\[([^\[]*)\]/, "");
-        if (ifComplex(key)) {
-            if (nextProp === "") nextProp = "0";
-            key = key.replace(/[^\[]+/, nextProp);
-            complex(key, value, (obj[prop] = resolveObj(obj[prop], nextProp).ob), doCoerce);
-        } else if (nextProp) {
-            const { ob, push } = resolveObj(obj[prop], nextProp);
-            obj[prop] = ob;
-            if (push) {
-                obj[prop].push({
-                    [nextProp]: (doCoerce ? coerce(value) : value)
-                });
-            } else {
-                obj[prop][nextProp] = (doCoerce ? coerce(value) : value);
-            }
-        } else {
-            simple([match[1], value], obj, true);
-        }
-    }
-}
 
+
+function complex(key, value, obj, doCoerce) {
+  doCoerce = setDefault(doCoerce, true);
+  var match = key.match(/([^\[]+)\[([^\[]*)\]/) || [];
+
+  if (match.length === 3) {
+    var prop = match[1];
+    var nextProp = match[2];
+    key = key.replace(/\[([^\[]*)\]/, "");
+
+    if (ifComplex(key)) {
+      if (nextProp === "") nextProp = "0";
+      key = key.replace(/[^\[]+/, nextProp);
+      complex(key, value, obj[prop] = resolveObj(obj[prop], nextProp).ob, doCoerce);
+    } else if (nextProp) {
+      var _resolveObj = resolveObj(obj[prop], nextProp),
+          ob = _resolveObj.ob,
+          push = _resolveObj.push;
+
+      obj[prop] = ob;
+
+      if (push) {
+        obj[prop].push(_defineProperty({}, nextProp, doCoerce ? coerce(value) : value));
+      } else {
+        obj[prop][nextProp] = doCoerce ? coerce(value) : value;
+      }
+    } else {
+      simple([match[1], value], obj, true);
+    }
+  }
+}
 /**
  * Handles simple query
  * @param {array} qArr 
  * @param {Object} queryObject 
  * @param {boolean} toArray 
  */
-function simple(qArr, queryObject, toArray, doCoerce) {
-    doCoerce = setDefault(doCoerce, true);
-    let key = qArr[0];
-    let value = qArr[1];
-    if (doCoerce) {
-        value = coerce(value);
-    }
-    if (key in queryObject) {
-        queryObject[key] = isArr(queryObject[key]) ? queryObject[key] : [queryObject[key]];
-        queryObject[key].push(value);
-    } else {
-        queryObject[key] = toArray ? [value] : value;
-    }
-}
 
+
+function simple(qArr, queryObject, toArray, doCoerce) {
+  doCoerce = setDefault(doCoerce, true);
+  var key = qArr[0];
+  var value = qArr[1];
+
+  if (doCoerce) {
+    value = coerce(value);
+  }
+
+  if (key in queryObject) {
+    queryObject[key] = isArr(queryObject[key]) ? queryObject[key] : [queryObject[key]];
+    queryObject[key].push(value);
+  } else {
+    queryObject[key] = toArray ? [value] : value;
+  }
+}
 /**
  * Restores values to their original type
  * @param {string} value undefined or string value
  */
-function coerce(value) {
-    if (value == null) return "";
-    if (typeof value !== "string") return value;
-    value = value.trim();
-    if (isNumber(value)) return +value;
-    switch (value) {
-        case "null": return null;
-        case "undefined": return undefined;
-        case "true": return true;
-        case "false": return false;
-        case "NaN": return NaN;
-        default: return value;
-    }
-}
 
-// Library encapsulation
+
+function coerce(value) {
+  if (value == null) return "";
+  if (typeof value !== "string") return value;
+  value = value.trim();
+  if (isNumber(value)) return +value;
+
+  switch (value) {
+    case "null":
+      return null;
+
+    case "undefined":
+      return undefined;
+
+    case "true":
+      return true;
+
+    case "false":
+      return false;
+
+    case "NaN":
+      return NaN;
+
+    default:
+      return value;
+  }
+} // Library encapsulation
+
+
 function lib() {
-    return deparam.apply(this, arguments);
+  return deparam.apply(this, arguments);
 }
 
 export default lib;
