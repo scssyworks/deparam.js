fab82490e6c611eacac69b8f2106e6ccde55751a
Sachin Singh <contactsachinsingh@gmail.com>
2023-03-18 23:24:30 +0530

    Version bumped

--- dist/umd/deparam.js
+++ dist/umd/deparam.js
@@ -3,7 +3,7 @@
  * Released under MIT license
  * @name Deparam.js
  * @author Sachin Singh <https://github.com/scssyworks/deparam.js>
- * @version 3.0.6
+ * @version 3.0.7
  * @license MIT
  */
 (function (global, factory) {
