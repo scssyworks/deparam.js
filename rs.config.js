61224d8951d37e520ddd087508afe9c247384cbb
Sachin Singh <contactsachinsingh@gmail.com>
2023-06-10 17:53:44 +0530

    Regenerated config file

---
+++ rs.config.js
@@ -0,0 +1,16 @@
+// Use "defineConfig" function for proper typescript intellisense while defining configuration
+const { defineConfig } = require('rollup');
+
+/**
+ * "rollup-scripts" defines a base rollup configuration.
+ * The base configuration should be sufficient for majority of applications.
+ * However, you can use this file to customise configuration according to your needs.
+ * @param baseConfig Initial rollup configuration
+ * @returns Rollup configuration
+ */
+module.exports = function config(baseConfig) {
+  return defineConfig({
+    ...baseConfig,
+    external: [],
+  });
+};
